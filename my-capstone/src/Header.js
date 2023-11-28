4cc4bc2a3965e09f31af87661d4e5a2f6dbf6ef1
noxolo <n.makhanya@lab.clicklearning.org>
2023-11-28 03:18:40 +0200

    commit

---
+++ my-capstone/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header () {
+   
+    
+    return (
+    <header className='header'>
+        <h2 className='header--title'>MovieLand</h2>
+        <div className="search--it">
+            <input type="text" placeholder="Search..."></input>
+        </div>
+   
+    </header>
+    )
+}
